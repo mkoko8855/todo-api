784cb25a3dbe8024df76fa97a609199de241e36b
mkoko8855 <mkoko8855@naver.com>
2023-07-05 15:26:23 +0900

    20230705

--- src/main/java/com/example/todo/HealthCheckController.java
+++ src/main/java/com/example/todo/HealthCheckController.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//0705
 @RestController
 @Slf4j
 public class HealthCheckController {
